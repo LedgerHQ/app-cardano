82a88cd3a53b8c74e56ed59f7625829f9d8adbdb
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2024-10-18 12:16:59 +0200

    Implement 1st ragger tests (Version, Serial, App Name)

---
+++ tests/utils.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2024 Ledger SAS
+# SPDX-License-Identifier: LicenseRef-LEDGER
+"""
+This module provides Ragger tests utility functions
+"""
+from pathlib import Path
+from typing import List
+import re
+
+
+ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
+
+
+def verify_version(version: str) -> None:
+    """Verify the app version, based on defines in Makefile
+
+    Args:
+        Version (str): Version to be checked
+    """
+
+    vers_dict = {}
+    vers_str = ""
+    lines = _read_makefile()
+    version_re = re.compile(r"^APPVERSION_(?P<part>\w)\s?=\s?(?P<val>\d)", re.I)
+    for line in lines:
+        info = version_re.match(line)
+        if info:
+            dinfo = info.groupdict()
+            vers_dict[dinfo["part"]] = dinfo["val"]
+    try:
+        vers_str = f"{vers_dict['M']}.{vers_dict['N']}.{vers_dict['P']}"
+    except KeyError:
+        pass
+    assert version == vers_str
+
+
+def verify_name(name: str) -> None:
+    """Verify the app name, based on defines in Makefile
+
+    Args:
+        name (str): Name to be checked
+    """
+
+    name_str = ""
+    lines = _read_makefile()
+    name_re = re.compile(r"^APPNAME\s*=\s*\"?(?P<val>[ \ta-zA-Z0-9_]+)\"?", re.I)
+    for line in lines:
+        info = name_re.match(line)
+        if info:
+            dinfo = info.groupdict()
+            name_str = dinfo["val"]
+            break
+    assert name == name_str
+
+
+def _read_makefile() -> List[str]:
+    """Read lines from the parent Makefile """
+
+    parent = Path(ROOT_SCREENSHOT_PATH).parent.resolve()
+    makefile = f"{parent}/Makefile"
+    print(f"Analyzing {makefile}...")
+    with open(makefile, "r", encoding="utf-8") as f_p:
+        lines = f_p.readlines()
+
+    return lines
